eb02d3416c7715b6823a56e5389c1fc8a8069faa
andpet0101 <andpet@gmx.de>
2024-04-09 19:40:57 +0200

    Fixed incorrect python path

--- R/general_configuration.R
+++ R/general_configuration.R
@@ -19,7 +19,6 @@ options(Seurat.object.assay.version="v5")
 
 # Python3 path needed for clustering, umap, other python packages
 reticulate_python3_path = unname(Sys.which("python3"))
-reticulate_python3_path = "C:\\Users\\andpe\\AppData\\Local\\Programs\\Python\\Python310\\python.exe"
 Sys.setenv(RETICULATE_PYTHON=reticulate_python3_path)
 
 # Buffer for reading large text files
